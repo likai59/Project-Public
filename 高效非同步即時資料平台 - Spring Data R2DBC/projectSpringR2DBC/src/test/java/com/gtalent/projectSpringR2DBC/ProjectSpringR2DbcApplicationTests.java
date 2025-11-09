01ffbdbb9226118e5e7ad225ffa6533bcdcce946
likai59 <likaihong59@gmail.com>
2025-11-09 14:29:19 +0800

    Create ProjectSpringR2DbcApplicationTests.java

---
+++ 高效非同步即時資料平台 - Spring Data R2DBC/projectSpringR2DBC/src/test/java/com/gtalent/projectSpringR2DBC/ProjectSpringR2DbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtalent.projectSpringR2DBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringR2DbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
